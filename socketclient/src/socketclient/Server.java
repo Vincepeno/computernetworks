6f8ef28f52dab1498b92d47cdf336d0559fa05d3
Vincepeno <vince-goossens@hotmail.com>
2014-02-22 21:00:20 +0100

    de server en de client al
    server eerst starten, dan client, kunt ge zo'n message typen en dan
    sluit de client ma server blijft bestaan.

---
+++ socketclient/src/socketclient/Server.java
@@ -0,0 +1,19 @@
+package socketclient;
+import java.io.*; 
+import java.net.*; 
+class Server 
+ { 
+ public static void main(String argv[]) throws Exception 
+ { 
+ ServerSocket welcomeSocket = new ServerSocket(6789); 
+ while(true) 
+ { 
+ Socket connectionSocket = welcomeSocket.accept(); 
+ BufferedReader inFromClient = new BufferedReader(new InputStreamReader (connectionSocket.getInputStream())); 
+ DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
+ String clientSentence = inFromClient.readLine(); 
+ System.out.println("Received: " + clientSentence); 
+ String capsSentence = clientSentence.toUpperCase() + '\n'; outToClient.writeBytes(capsSentence); 
+ } 
+ } 
+ } 
