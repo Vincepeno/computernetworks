549e5eb733fcb060ca3abc4ed6daad67e8c4bb54
Wouter <Wouter@10.36.44.17>
2014-03-03 00:37:53 +0100

    Example class + ServerParser aangepast

---
+++ socketclient/src/socketclient/Example.java
@@ -0,0 +1,21 @@
+package socketclient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+public class Example {
+	public static void main(String argv[]) throws Exception {
+		Socket s = new Socket(InetAddress.getByName("wikipedia.be"), 80);
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		pw.println("GET / HTTP/1.1");
+		pw.println("Host: wikipedia.be");
+		pw.println();
+		pw.flush();
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String t;
+		while((t = br.readLine()) != null) System.out.println(t);
+		br.close();
+	}
+}
